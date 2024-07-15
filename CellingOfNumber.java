7c96fc44bbde1481d288f8c5da350d9217a54460
KIRITI AAJAD <112309709+Kiritiaajd@users.noreply.github.com>
2024-07-15 09:17:54 +0530

    modified

--- CellingOfNumber.java
+++ CellingOfNumber.java
@@ -1,9 +1,26 @@
 public class CellingOfNumber {
 
     public static void main(String[] args) {
-                int arr[] = {2,3,5,9,14,16,18};
-                int target = 15;
-                System.out.println("Celling of "+target+ " is "+CellingNumber(arr, target)   );
+        int arr[] = { 2, 3, 5, 9, 14, 16, 18 };
+        int target = 15;
+        System.out.println("Celling of " + target + " is " + CellingNumber(arr, target));
+        System.out.println("Floor of " + target + " is " + FloorNumber(arr, target));
+    }
+
+    static int FloorNumber(int arr[], int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return arr[mid];
+            }
+        }
+        return arr[end];
     }
 
     static int CellingNumber(int[] arr, int target) {
@@ -12,7 +29,7 @@ public class CellingOfNumber {
         while (start <= end) {
             int mid = start + (end - start) / 2;
             if (target < arr[mid]) {
-                end = mid -1;
+                end = mid - 1;
             } else if (target > arr[mid]) {
                 start = mid + 1;
             } else {
